4c7fcc4f6fb5c6b06cce99225124dfd46826585f
Xiao Xia <xiaoxia@hiretual.com>
2020-11-07 00:08:31 +0800

    xx21: unify user list

--- frontend/src/components/chat/UserList/index.js
+++ frontend/src/components/chat/UserList/index.js
@@ -1,47 +1,84 @@
-import {  List, Avatar, Button } from 'antd';
-import inviteForm from "../inviteForm"
+import React from "react";
+import { List, Avatar, Button } from "antd";
+import inviteForm from "../inviteForm";
 import webSocket from "../../websocket/Websocket";
+import colorHelper from "../../../helpers/color-user-helper";
 
 const UserList = () => {
-    const data = [
-        {"name":{"title":"Miss","first":"Weiwei","last":"Zhou"},"school":"Rice University","interest":"Reading"},
-        {"name":{"title":"Mr","first":"Xiao","last":"Xia"},"school":"Rice University","interest":"Sports"},
-        {"name":{"title":"Mr","first":"Zhijian","last":"Yao"},"school":"Rice University","interest":"Traveling"},
-        {"name":{"title":"Mr","first":"Xuyang","last":"Xiao"},"school":"Rice University","interest":"Sports"},
-        {"name":{"title":"Mr","first":"Wenlong","last":"Yan"},"school":"Rice University","interest":"Sports"},
-        {"name":{"title":"Miss","first":"Xinru","last":"Xiao"},"school":"Rice University","interest":"Sports"},
-    ]
-    const ColorList = ['#f56a00', '#7265e6', '#ffbf00', '#00a2ae'];
+  const data = [
+    {
+      name: { title: "Miss", first: "Weiwei", last: "Zhou" },
+      school: "Rice University",
+      interest: "Reading"
+    },
+    {
+      name: { title: "Mr", first: "Xiao", last: "Xia" },
+      school: "Rice University",
+      interest: "Sports"
+    },
+    {
+      name: { title: "Mr", first: "Zhijian", last: "Yao" },
+      school: "Rice University",
+      interest: "Traveling"
+    },
+    {
+      name: { title: "Mr", first: "Xuyang", last: "Xiao" },
+      school: "Rice University",
+      interest: "Sports"
+    },
+    {
+      name: { title: "Mr", first: "Wenlong", last: "Yan" },
+      school: "Rice University",
+      interest: "Sports"
+    },
+    {
+      name: { title: "Miss", first: "Xinru", last: "Xiao" },
+      school: "Rice University",
+      interest: "Sports"
+    }
+  ];
+  const ColorList = ["#f56a00", "#7265e6", "#ffbf00", "#00a2ae"];
 
-    return (
-        <div>
-        <List
-            itemLayout="horizontal"
-            dataSource={data}
-            renderItem={item => (
-                <List.Item style={{padding: '15px'}}>
+  return (
+    <div>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={item => (
+          <List.Item style={{ padding: "15px" }}>
+            <List.Item.Meta
+              avatar={
+                <Avatar
+                  style={{
+                    backgroundColor: colorHelper(
+                      `${item.name.first} ${item.name.last}`
+                    ),
+                    verticalAlign: "middle"
+                  }}
+                  size="large"
+                  gap={1}
+                >
+                  {item.name.first[0] + item.name.last[0]}
+                </Avatar>
+              }
+              description={item.name.first + " " + item.name.last}
+              title={item.school}
+            />
 
-                    <List.Item.Meta
-                        avatar={      <Avatar style={{ backgroundColor: ColorList[Math.floor(Math.random()*5)], verticalAlign: 'middle' }} size="large" gap={4}>
-                            {item.name.first[0]}
-                        </Avatar>}
-                        description={item.name.first + " " +item.name.last}
-                        title = {item.school}
-                    />
-
-                    <Button type="primary" shape="round" size='small'
-                            onClick={() => alert("to complete")}>
-                        Chat!
-                    </Button>
-
-                </List.Item>
-            )}
-        />
-            {inviteForm()}
-        </div>
-
-
-    )
-}
+            <Button
+              type="primary"
+              shape="round"
+              size="small"
+              onClick={() => alert("to complete")}
+            >
+              Chat!
+            </Button>
+          </List.Item>
+        )}
+      />
+      {inviteForm()}
+    </div>
+  );
+};
 
 export default UserList;
