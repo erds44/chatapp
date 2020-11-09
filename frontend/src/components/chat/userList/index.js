905cbaac49ba0efb7063b98b212aea4f6a2c6c9a
xinruxiao <56401165+xinruxiao@users.noreply.github.com>
2020-11-09 11:44:01 -0600

    delete navigate, disabled button, add scroller

--- frontend/src/components/chat/userList/index.js
+++ frontend/src/components/chat/userList/index.js
@@ -6,10 +6,12 @@ import Message from "./message";
 import {connect, useSelector} from "react-redux";
 import ChatMessage from "../chat-area/ChatMessage";
 
+
 const UserList = (props) => {
 
     const [visible, setVisible] = useState(false);
     const [userName, setUserName] = useState("");
+
 
     const allUsers = useSelector( state => state.userList.allUserList);
     const currentUser = useSelector (state => state.login.user);
@@ -17,47 +19,56 @@ const UserList = (props) => {
         return sender.split("_").map(name => name[0]);
     };
     console.log(currentUser)
+
     return (
     <div>
         <Message userName={userName} visible={visible} setVisible={setVisible}/>
-      <List
-        itemLayout="horizontal"
-        dataSource={allUsers}
-        renderItem={item => (
-          <List.Item style={{ padding: "15px" }}>
-            <List.Item.Meta
-              avatar={
-                <Avatar
-                  style={{
-                    backgroundColor: colorHelper(
-                      item.name
-                    ),
-                    verticalAlign: "middle"
-                  }}
-                  size="large"
-                  gap={1}
-                >
-                  {getDisplayName(item.name)}
-                </Avatar>
-              }
-              description={item.name}
-              title={item.school}
+            <List
+                style = {{width:"100%", height:"100%",position:"absolute", overflow:"scroll"}}
+                itemLayout="horizontal"
+                dataSource={allUsers}
+                renderItem={item => (
+                    <List.Item style={{ padding: "15px" }}>
+                        <List.Item.Meta
+                            avatar={
+                                <Avatar
+                                    style={{
+                                        backgroundColor: colorHelper(
+                                            item.name
+                                        ),
+                                        verticalAlign: "middle"
+                                    }}
+                                    size="large"
+                                    gap={1}
+                                >
+                                    {getDisplayName(item.name)}
+                                </Avatar>
+                            }
+                            description={item.name}
+                            title={item.school}
+                        />
+                        {item.name != currentUser?
+                            <Button
+                                type="primary"
+                                shape="round"
+                                size="small"
+                                onClick={() => {setUserName(item.name); setVisible(true)}}
+                            >
+                                Chat!
+                            </Button>
+                            :   <Button
+                                type="primary"
+                                shape="round"
+                                size="small"
+                                disabled = {true}
+                            >
+                                Chat!
+                            </Button>}
+
+
+                    </List.Item>
+                )}
             />
-              {item.name != currentUser?
-                  <Button
-                  type="primary"
-                  shape="round"
-                  size="small"
-                  onClick={() => {setUserName(item.name); setVisible(true)}}
-              >
-                  Chat!
-              </Button>
-              :  null}
-
-
-          </List.Item>
-        )}
-      />
     </div>
   );
 };
