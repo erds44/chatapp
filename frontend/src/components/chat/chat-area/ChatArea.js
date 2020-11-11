27bef5d10153b1754c0e367af857e54d20683ef0
Xiao Xia <xiaoxia@hiretual.com>
2020-11-11 22:47:28 +0800

    xx21: improve scroll

--- frontend/src/components/chat/chat-area/ChatArea.js
+++ frontend/src/components/chat/chat-area/ChatArea.js
@@ -11,11 +11,22 @@ const ChatArea = ({ chatRoom }) => {
   const messages = useSelector(state => {
     return state.message.messages;
   });
+  const [userScrolled, setUserScrolled] = useState(false);
+  useEffect(() => {
+    const chatAreaContent = document.querySelector("#chat-area-content");
+    if (
+      chatAreaContent.scrollTop >
+      chatAreaContent.scrollHeight - 1.5 * chatAreaContent.clientHeight
+    ) {
+      chatAreaContent.scrollTop =
+        chatAreaContent.scrollHeight - chatAreaContent.clientHeight;
+    }
+  }, [messages]);
 
   return (
     <div id={"chat-area"}>
       <Layout>
-        <Content>
+        <Content id={"chat-area-content"}>
           <ChatMessages inputMessages={messages[chatRoom]} />
         </Content>
         <Footer>
